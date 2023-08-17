3ab5c8300f77e3d98ca6e05a840689b57a9aa327
webkong <772930508@qq.com>
2023-08-17 14:58:11 +0800

    feat: remove console.log

--- rollup.config.js
+++ rollup.config.js
@@ -1,57 +1,60 @@
-const path = require("path");
-const { babel } = require("@rollup/plugin-babel");
-const { nodeResolve } = require("@rollup/plugin-node-resolve");
-const commonjs = require("@rollup/plugin-commonjs");
-const vue = require("rollup-plugin-vue");
-const css = require("rollup-plugin-css-only");
-const { terser } = require("rollup-plugin-terser");
-const image = require("@rollup/plugin-image");
-const clear = require("rollup-plugin-clear");
+const path = require('path');
+const { babel } = require('@rollup/plugin-babel');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const vue = require('rollup-plugin-vue');
+const css = require('rollup-plugin-css-only');
+const { terser } = require('rollup-plugin-terser');
+const image = require('@rollup/plugin-image');
+const clear = require('rollup-plugin-clear');
 
 const resolveFile = function (filePath) {
-  return path.join(__dirname, ".", filePath);
+  return path.join(__dirname, '.', filePath);
 };
 const babelOptions = {
-  babelHelpers: "bundled",
-  presets: ["@babel/preset-env"],
+  babelHelpers: 'bundled',
+  presets: ['@babel/preset-env'],
 };
 const plugins = [
   clear({
-    targets: ["dist"],
+    targets: ['dist'],
   }),
   vue({ css: false }),
-  css({ output: "toast.css" }),
+  css({ output: 'toast.css' }),
   image(),
   nodeResolve(),
   commonjs(),
   babel(babelOptions),
-  terser(),
+  terser({
+    // 编译时，去掉console.log
+    compress: { drop_console: true },
+  }),
 ];
 module.exports = [
   {
-    input: resolveFile("index.js"),
+    input: resolveFile('index.js'),
     output: {
-      file: resolveFile("dist/index.umd.js"),
-      format: "umd",
-      name: "WKToast",
+      file: resolveFile('dist/index.umd.js'),
+      format: 'umd',
+      name: 'WKToast',
       globals: {
-        vue: "Vue",
+        vue: 'Vue',
       },
     },
     plugins: plugins,
-    external: ["vue"],
+    external: ['vue'],
   },
   {
-    input: resolveFile("index.js"),
+    input: resolveFile('index.js'),
     output: {
-      file: resolveFile("dist/index.esm.js"),
-      format: "esm",
-      name: "WKToast",
+      file: resolveFile('dist/index.esm.js'),
+      format: 'esm',
+      name: 'WKToast',
       globals: {
-        vue: "Vue",
+        vue: 'Vue',
       },
     },
     plugins: plugins,
-    external: ["vue"],
+    external: ['vue'],
   },
 ];
